866fb2c8b4d43502655a3cfce7a11c2879f8fca6
戴亮金 <707689817@qq.com>
2017-07-31 16:23:31 +0800

    1_6.0 OC代码混淆脚本Demo

---
+++ 1_6.0/HSKConfuse/HSKConfuse/Resource/codeObfuscation.h
@@ -0,0 +1,7 @@
+#ifndef Demo_codeObfuscation_h
+#define Demo_codeObfuscation_h
+//confuse string at Mon Jul 31 16:22:20 CST 2017
+#define hsk_Function1 xjTCWHRYngWxShRg
+#define hsk_Function2 iAfNIojzUYwcNvMG
+#define hsk_start eADlnYkbXDSuziMl
+#endif
---
+++ 1_6.0/HSKConfuse/HSKConfuse/ViewController.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  HSKConfuse
+//
+//  Created by Code_Hou on 2017/3/11.
+//  Copyright © 2017年 侯森魁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+-(void)Hsk_Function2{
+}
+-(void)hsk_Function2{
+    
+}
+- (void)handXXX{
+    
+}
+- (void)hand{
+    
+}
+- (void)hsk_start{
+    
+}
+- (void)hfunction{
+    
+}
+- (void)_hou{
+    
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
